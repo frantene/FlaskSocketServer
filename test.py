c30fc35f24dfa1547c880979032c7768125c193d
GamerVerse722 <cristian987654321rios@gmail.com>
2024-03-26 16:34:30 -0500

    Inheritance test

--- test.py
+++ test.py
@@ -1,28 +1,42 @@
 import pprint
+
 class Test:
     def __init__(self):
         self.main = [True]
         self.other = "String is inherited"
-        self.inheritance = self.Inheritance(self.main)
+        self.room = {}
+
+    def create_room(self, room_code):
+        self.room[room_code] = {
+            "Inheritance Data": self.Inheritance(room=self.room, main=self.main)
+        }
 
     class Inheritance:
-        def __init__(self, main):
+        def __init__(self, room=None, main='Temporary'):
             self.main = main
             self.main.append('Hi')
+            self.current = room
 
         def add_data(self, number):
             self.main.append(number)
 
+        def set_room(self, code):
+            self.current = code
+
     def data(self):
         return {'Main': self.main,
-                'Inherited': self.other}
+                'other': self.other,
+                'Inheritance': self.room}
 
 # Create an instance of the Test class
 test_instance = Test()
 
-# Call the add_data method
-test_instance.inheritance.add_data(5)
+# Call the create_room method
+test_instance.create_room('Among Us')
 
 # Access the data using the data() method
-pprint.pp(test_instance.data(), indent=2)
+pprint.pprint(test_instance.data(), indent=2)
 
+# Accessing and running a function in the Inheritance class
+test_instance.room['Among Us']["Inheritance Data"].add_data("New data")
+print(test_instance.room['Among Us']["Inheritance Data"].main)
